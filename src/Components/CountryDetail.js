cabdb2b1dfcb9f0fbaad71e374a0bf8fc9d6f25f
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-06-16 22:02:17 +0100

    flag link added

--- src/Components/CountryDetail.js
+++ src/Components/CountryDetail.js
@@ -4,8 +4,8 @@ const CountryDetail = ({country}) => {
     return(
         <div>
             <h3>{country.name}</h3>
-            <p>Languages: {country.languages.name}</p>
-            <p>Flag: {country.flag}</p>
+            {/* <p>Languages: {country.name.languages.name}</p>   */}
+            <p><a href={country.flag}>Flag</a> </p>
         </div>
     )
 }
