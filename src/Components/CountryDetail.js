f1a4eb3afb374ed5b566276648ec150fa62912ae
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-06-17 08:38:27 +0100

    tested

--- src/Components/CountryDetail.js
+++ src/Components/CountryDetail.js
@@ -4,7 +4,7 @@ const CountryDetail = ({country}) => {
     return(
         <div>
             <h3>{country.name}</h3>
-            {/* <p>Languages: {country.name.languages.name}</p>   */}
+            {/* <p>Languages: {country.languages.name}</p>   */}
             <p><a href={country.flag}>Flag</a> </p>
         </div>
     )
